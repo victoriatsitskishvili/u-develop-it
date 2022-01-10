6d6bc6b6481c651077db9b018acf55f09f846c02
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-01-10 18:58:24 -0500

    adding password to SQL

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
+  port: 3306,
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: '',
+  password: 'password',
   database: 'election'
 });
 
