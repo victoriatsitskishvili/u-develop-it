6ee31850d96d5ac06e175ee6024030c6cca33c55
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-01-09 21:55:55 -0500

    added voter routes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,5 @@
-INSERT INTO voters (first_name, last_name, email)
+INSERT INTO voters
+  (first_name, last_name, email)
 VALUES
   ('James', 'Fraser', 'jf@goldenbough.edu'),
   ('Jack', 'London', 'jlondon@ualaska.edu'),
@@ -51,13 +52,15 @@ VALUES
   ('William', 'Carleton', 'wcarleton@literature.com'),
   ('Gerald', 'Griffin', 'ggriff@lit.net');
 
-INSERT INTO parties (name, description)
+INSERT INTO parties
+  (name, description)
 VALUES
-  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'), 
-  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'), 
+  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
+  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
   ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
 
-INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
+INSERT INTO candidates
+  (first_name, last_name, party_id, industry_connected)
 VALUES
   ('Ronald', 'Firbank', 1, 1),
   ('Virginia', 'Woolf', 1, 1),
